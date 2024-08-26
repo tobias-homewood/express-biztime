4cf0d0efaf657bcacbba78be1196cd79e1720021
tobias-homewood <toby_homewood@hotmail.co.uk>
2024-08-26 16:36:09 +0100

    db setup

--- db.js
+++ db.js
@@ -1,2 +1,28 @@
 /** Database setup for BizTime. */
+const { Client } = require("pg");
+require("dotenv").config();
 
+let DB_URI = process.env.DB_URI;
+
+if (DB_URI === undefined) {
+    console.error("No DB_URI found; exiting");
+    process.exit(1);
+}
+
+let db = new Client({
+    connectionString: DB_URI,
+});
+
+db.connect();
+
+// test connection
+db.query("SELECT NOW()", (err, res) => {
+    if (err) {
+        console.error("Error connecting to database");
+        console.error(err);
+    } else {
+        console.log("Connected to database");
+    }
+});
+
+module.exports = db;
