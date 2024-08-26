3746ee9e26895461958d66222126ac7b0b01fff5
tobias-homewood <toby_homewood@hotmail.co.uk>
2024-08-26 16:40:35 +0100

    db import

--- app.js
+++ app.js
@@ -1,31 +1,29 @@
 /** BizTime express application. */
 
-
 const express = require("express");
+const db = require("./db");
 
 const app = express();
-const ExpressError = require("./expressError")
+const ExpressError = require("./expressError");
 
 app.use(express.json());
 
-
 /** 404 handler */
 
-app.use(function(req, res, next) {
-  const err = new ExpressError("Not Found", 404);
-  return next(err);
+app.use(function (req, res, next) {
+    const err = new ExpressError("Not Found", 404);
+    return next(err);
 });
 
 /** general error handler */
 
 app.use((err, req, res, next) => {
-  res.status(err.status || 500);
+    res.status(err.status || 500);
 
-  return res.json({
-    error: err,
-    message: err.message
-  });
+    return res.json({
+        error: err,
+        message: err.message,
+    });
 });
-
 
 module.exports = app;
